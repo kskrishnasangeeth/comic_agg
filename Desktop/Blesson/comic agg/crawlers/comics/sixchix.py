b61c29989d5bb9bbf490670d05cee98831e952cf
root <root@krishna-Vostro-A860.(none)>
2012-08-26 14:57:54 +0530

    version 1.0

--- Desktop/Blesson/comic agg/crawlers/comics/sixchix.py
+++
@@ -1,6 +0,0 @@
-from crawlers.base import BaseComicCrawler
-
-class SixChix(BaseComicCrawler):
-	def __init__(self):
-		super(SixChix, self).__init__('Six Chix', 'cserver.it-content.com',80, 'Norske')
-		self.url = "http://cserver.it-content.com/retriever.php?id=31&date=%s" % (self.date.strftime("%Y%m%d"))
